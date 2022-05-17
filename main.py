d019fca9e4d7aa35c6f85445f1a31997fa4c617a
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-05-17 16:53:52 -0500

    init

---
+++ main.py
@@ -0,0 +1,4 @@
+import glob                         #Recorrer achivos
+import pandas as pd                 
+import re                           #Regular expressions
+import os                           #Llamados al sistema
