4f41ea0663b2176e959f37eb8d3211a14e557b6d
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-05-17 17:12:57 -0500

    Archivos de prueba creados

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import glob                         #Recorrer achivos
-import pandas as pd                 
+import pandas as pd                 #Dataframes
 import re                           #Regular expressions
 import os                           #Llamados al sistema
+
+l_extensiones = ['.csv', '.xlsx', '.xls', '.txt']
+
