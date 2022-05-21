344c07e637fad4b7bd2d7ad8fd4608d9a4f0d0a2
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-05-20 21:48:58 -0500

    Busca hasta dentro de subdirectorios dentro de la ruta relativa donde está el programa

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from fileinput import filename
-import glob
-from msilib.schema import File                                             #Recorrer achivos
+import glob                                             #Recorrer achivos
 import pandas as pd                                     #Dataframes
 import re                                               #Regular expressions
 import os                                               #Llamados al sistema
@@ -8,11 +6,14 @@ import os                                               #Llamados al sistema
 l_extensiones = ['.csv', '.xlsx', '.xls', '.txt']       #Lista extensiones de archivos que se pueden buscar
 l_searchWords = []                                      #Lista de palabras clave a buscar
 
+#Función que encuentra que archivos tienen la extensión entregada
 def file_mapping(ext, num):
-    str1 = 'test_folder/*'
+    str1 = '**/*'
     str2 = str1 + ext
     for fileName in glob.glob(str2, recursive=True):
-        print('- {}'.format(fileName))
+        if(fileName == 'search_words.txt'):
+            continue
+        print('- {}'.format(fileName))      
         if(num == 1):
             try:
                 for w in  l_searchWords:
@@ -38,25 +39,22 @@ def file_mapping(ext, num):
             except:
                 print('\n[Error]: search_txt falló')  
 
-
+# Buscan de forma distinta para cada tipo de archivo
 def search_csv(csvName, word):
     df = pd.read_csv(csvName, sep=';')
     count = len(re.findall(word, df.to_string().upper()))
     if(count > 0):
         print(('    Veces que se repite \'{}\': {}'.format(word, count)))
-
 def search_excel(excelName, word):
     df = pd.read_excel(excelName, sheet_name='Hoja1')
     count = len(re.findall(word, df.to_string().upper()))
     if(count > 0):
         print('    Veces que se repite \'{}\': {}'.format(word, count))
-
 def search_xls(excelName, word):
     df = pd.read_excel(excelName, engine='xlrd')
     count = len(re.findall(word, df.to_string().upper()))
     if(count > 0):
         print('    Veces que se repite \'{}\': {}'.format(word, count))
-
 def search_txt(txtName, word):
     txtFile = open(txtName)
     lines = txtFile.readlines()
@@ -68,6 +66,7 @@ def search_txt(txtName, word):
     if(count > 0):
         print('   Veces que se repite \'{}\': {}'.format(word, count))
 
+# Carga palabras clave del archivo de texto
 def load_words():
     txtWords = open('search_words.txt')
     lines = txtWords.readlines()
@@ -88,22 +87,19 @@ def main():
                 break
             if(ext < 1 or ext > 5):
                 print("\n[Error]: Ingrese un valor valido")
-                valid = -1
+                continue          
         except:
             print("\n[Error]: Ingrese un valor númerico")
-            valid = -1
-        if(valid == 1):   
-            try:
-                if (ext > 0 and ext < 5):
-                    file_mapping(l_extensiones[ext-1], ext)
-                elif ext == 5:
-                    it = 0
-                    for e in l_extensiones:
-                        it += 1
-                        file_mapping(e, it)
-                else:
-                    print("\n[Error]: Ingrese una opción valida")
-            except:
-                print('\n[Error]: file_mapping() falló')
+            continue       
+        try:
+            if (ext > 0 and ext < 5):
+                file_mapping(l_extensiones[ext-1], ext)
+            elif ext == 5:
+                it = 0
+                for e in l_extensiones:
+                    it += 1
+                    file_mapping(e, it)
+        except:
+            print('\n[Error]: file_mapping() falló')
     print('\nEND')
 main()
