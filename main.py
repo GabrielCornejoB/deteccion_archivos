b1ff9720caaba1d9bc1f6cfd038e2743a248d1d2
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-05-22 11:17:56 -0500

    avances dir raiz

--- main.py
+++ main.py
@@ -2,62 +2,98 @@ import glob                                             #Recorrer achivos
 import pandas as pd                                     #Dataframes
 import re                                               #Regular expressions
 import os                                               #Llamados al sistema
+import warnings
 
 l_extensiones = ['.csv', '.xlsx', '.xls', '.txt']       #Lista extensiones de archivos que se pueden buscar
 l_searchWords = []                                      #Lista de palabras clave a buscar
 
+warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
+
 #Función que encuentra que archivos tienen la extensión elegida
-def file_mapping(ext, num):
-    str1 = '**/*'
-    str2 = str1 + ext
-    for fileName in glob.glob(str2, recursive=True):
+def file_mapping(ext, num, loc):
+    l_path = ['C:/Users/', os.getlogin(),'/']
+    str1 = ''.join([str(elem) for elem in l_path])
+    if(loc == 1):
+        str2 = 'Downloads/**/*'
+    elif(loc == 2):
+        str2 = 'Desktop/**/*'
+    elif(loc == 3):
+        str2 = 'Documents/**/*'
+    str3 = str1 + str2 + ext
+    if(loc == 5):
+        str3 = 'C:/**/*'
+    search = glob.glob(str3, recursive=True)
+    print("\n\n")
+    print('-'*75)
+    print("{} archivos encontrados con extensión \'{}\'\n".format(len(search), ext))
+    for fileName in search:
         if(fileName == 'search_words.txt'):
             continue
         print('- {}'.format(fileName))      
         if(num == 1):
             try:
                 for w in  l_searchWords:
-                    search_csv(fileName, w.upper())
-            except:
-                print('\n[Error]: search_csv falló')
+                    if(search_csv(fileName, w.upper()) == -1):
+                        break
+            except Exception as e:
+                print('\n[Error]: search_csv falló {}'.format(e))
         elif(num == 2):
             try:
                 for w in  l_searchWords:
-                    search_excel(fileName, w.upper())
-            except:
-                print('\n[Error]: search_excel falló')
+                    if(search_excel(fileName, w.upper()) == -1):
+                        break
+            except Exception as e:
+                print('\n[Error]: search_excel falló {}'.format(e))
         elif(num == 3):
             try:
                 for w in  l_searchWords:
-                    search_xls(fileName, w.upper())
-            except:
-                print('\n[Error]: search_xls falló')
+                    if(search_xls(fileName, w.upper()) == -1):
+                        break
+            except Exception as e:
+                print('\n[Error]: search_xls falló {}'.format(e))
         elif(num == 4):
             try:
                 for w in  l_searchWords:
-                    search_txt(fileName, w.upper())
-            except:
-                print('\n[Error]: search_txt falló')  
+                    if(search_txt(fileName, w.upper()) == -1):
+                        break
+            except Exception as e:
+                print('\n[Error]: search_txt falló {}'.format(e))  
 
 # Buscan de forma distinta para cada tipo de archivo
 def search_csv(csvName, word):
-    df = pd.read_csv(csvName, sep=';')
+    try:
+        df = pd.read_csv(csvName, sep=';')
+    except Exception as e:
+        # print('\n[ERROR]: read_csv falló {}'.format(e))
+        return -1
     count = len(re.findall(word, df.to_string().upper()))
     if(count > 0):
         print(('    Veces que se repite \'{}\': {}'.format(word, count)))
 def search_excel(excelName, word):
-    df = pd.read_excel(excelName, sheet_name='Hoja1')
+    try:
+        df = pd.read_excel(excelName)
+    except Exception as e:
+        # print('\n[ERROR]: read_excel falló {}'.format(e))
+        return -1   
     count = len(re.findall(word, df.to_string().upper()))
     if(count > 0):
         print('    Veces que se repite \'{}\': {}'.format(word, count))
 def search_xls(excelName, word):
-    df = pd.read_excel(excelName, engine='xlrd')
+    try:
+        df = pd.read_excel(excelName, engine='xlrd')
+    except Exception as e:
+        # print('\n[ERROR]: read_excel (xls) falló {}'.format(e))
+        return -1
     count = len(re.findall(word, df.to_string().upper()))
     if(count > 0):
         print('    Veces que se repite \'{}\': {}'.format(word, count))
 def search_txt(txtName, word):
-    txtFile = open(txtName)
-    lines = txtFile.readlines()
+    try:
+        txtFile = open(txtName)
+        lines = txtFile.readlines()
+    except Exception as e:
+        print('\n[ERROR]: archivo no valido'.format())
+        return -1
     count = 0
     for line in lines:
         times = len(re.findall(word, line.upper()))
@@ -72,7 +108,6 @@ def load_words():
     lines = txtWords.readlines()
     for line in lines:
         l_searchWords.append(line.strip())
-    print(l_searchWords)
 
 def main():
     ext = 0
@@ -83,22 +118,48 @@ def main():
             ext = int(extension)
             if(ext == 0):
                 print('\n[EXIT]')
-                break
+                return
             if(ext < 1 or ext > 5):
                 print("\n[Error]: Ingrese un valor valido")
                 continue          
         except:
             print("\n[Error]: Ingrese un valor númerico")
-            continue       
+            continue 
+    location = 0
+    while(location < 1 or location > 5):
+        location = input('\nIngrese el # correspondiente a la carpeta donde desea buscar (o \'0\' para salir):\n(1) Descargas\n(2) Escritorio\n(3) Documentos\n(4) Todas las anteriores\n(5) Todo el disco C:\n')   
         try:
+            location = int(location)
+            if(ext == 0):
+                print('\n[EXIT]')
+                return
+            if(location < 1 or location > 5):
+                print("\n[Error]: Ingrese un valor valido")
+                continue
+        except:
+            print("\n[Error]: Ingrese un valor númerico")
+            continue
+    try:
+        if(location > 0 and location < 6):
             if (ext > 0 and ext < 5):
-                file_mapping(l_extensiones[ext-1], ext)
+                file_mapping(l_extensiones[ext-1], ext, location)
             elif ext == 5:
                 it = 0
                 for e in l_extensiones:
                     it += 1
-                    file_mapping(e, it)
-        except:
-            print('\n[Error]: file_mapping() falló')
+                    file_mapping(e, it, location)
+        elif location == 4:
+            l_l = ['DOWNLOADS', 'DESKTOP', 'DOCUMENTS']
+            for l in range(3):
+                print('\n\n\t\t\t\t[{}]\n'.format(l_l[l]))
+                if (ext > 0 and ext < 5):
+                    file_mapping(l_extensiones[ext-1], ext, l+1)
+                elif ext == 5:
+                    it = 0
+                    for e in l_extensiones:
+                        it += 1
+                        file_mapping(e, it, l+1)
+    except Exception as e:
+        print('\n[Error]: file_mapping() falló. {}'.format(e))
     print('\nEND')
 main()
