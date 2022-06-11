9abb522ca0f0142da7f1b9bb07edd0ad4fe57249
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-06-11 16:38:40 -0500

    lista global

--- main.py
+++ main.py
@@ -6,35 +6,37 @@ import warnings                                                 #Ignorar warning
 l_exts = ['.csv','.xlsx','.xls','.txt']                         #Lista con las extensiones de los archivos a buscar
 l_searchWords = []                                              #Lista de palabras clave a buscar
 l_sumFiles = []                                                 #Lista de todos los archivos para el resumen
+# Lista donde se adicionaran las distintas cadenas, esta luego será retornada
+l_return_str = []
 
 warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 
 def search_words(path, word):
-    l_s = []
+    l_return_str = []
     files = []
     for e in l_exts:
         tmp_path =  path + '/**/*' + e
         files.extend(glob.glob(tmp_path, recursive=True))
-    l_s.append("La palabra " + word.upper() + " se encuentra en los siguientes archivos:")
+    l_return_str.append("La palabra " + word.upper() + " se encuentra en los siguientes archivos:")
         
     for filename in files:
         if(filename.lower().endswith(".csv")):
             search = search_csv(filename, word.upper())
             if(search > 0):
-                l_s.append(' - ' + filename)
+                l_return_str.append(' - ' + filename)
         elif(filename.lower().endswith(".xlsx")):
             search = search_xlsx(filename, word.upper())
             if(search > 0):
-                l_s.append(' - ' + filename)
+                l_return_str.append(' - ' + filename)
         elif(filename.lower().endswith(".xls")):
             search = search_xls(filename, word.upper())
             if(search > 0):
-                l_s.append(' - ' + filename)
+                l_return_str.append(' - ' + filename)
         elif(filename.lower().endswith(".txt")):
             search = search_txt(filename, word.upper())
             if(search > 0):
-                l_s.append(' - ' + filename)
-    return l_s
+                l_return_str.append(' - ' + filename)
+    # return l_s
 
 def search_csv(csvName, word):
     try:
@@ -77,14 +79,13 @@ def search_txt(txtName, word):
     return count
 
 def start_search(word, path):
-    # Lista donde se adicionaran las distintas cadenas, esta luego será retornada
-    l_return_str = []
+    
     # Mensaje de inicio para que el servidor sepa cuando se debe crear un archivo de output nuevo
     l_return_str.append('[START]')
     # Indica en el output, la busqueda que se realizó para recibir ese resultado
     l_return_str.append("BUSQUEDA: Ruta: " + path + " Palabra: " + word + '\n')
     # Busca según la ruta y la palabra entregada, en proceso la busqueda de varias palabras
-    l_return_str.extend(search_words(path, word))
+    search_words(path, word)
     # Mensaje de fin para que el servidor sepa cuando debe cerrar el archivo de output
     l_return_str.append('[END]')
     # Retorna la lista con el output del programa
